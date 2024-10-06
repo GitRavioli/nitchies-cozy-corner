d06c82ac1d059cee42f2753355de0dd7cd0b1697
ImNitch <nyychttv21@gmail.com>
2024-10-06 13:51:46 -0400

    Enabled speed insight functionality.

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 
 const app = createApp(App);
 
 inject();
+injectSpeedInsights();
 
 app.use(router);
 
